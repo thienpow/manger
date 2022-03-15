debe4599f3a70b317826548a7dc9c5fcbd228ef9
Ken Chong <thienpow@aadi.my>
2022-03-15 11:18:12 +0000

    rename folder

--- copy_deploy.sh
+++ copy_deploy.sh
@@ -2,9 +2,9 @@
 trunk build --release
  
 cd ../
-rm -rf manger_deploy/*
-rsync -a manger2/dist/ manger_deploy/
-cd manger_deploy
+rm -rf manger-deploy/*
+rsync -a manger2/dist/ manger-deploy/
+cd manger-deploy
 git add .
 git commit -m "update"
 git push
