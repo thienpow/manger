1adfad080f647d583e276a84a06055b08629f46a
Ken Chong <thienpow@aadi.my>
2022-03-18 16:42:01 +0800

    minor fix

--- copy_deploy.sh
+++ copy_deploy.sh
@@ -2,11 +2,11 @@
 trunk build --release
  
 cd ../
-cd manger-deploy
-git pull
-cd ../
-rm -rf manger-deploy/*
-rsync -a manger2/dist/ manger-deploy/
+#cd manger-deploy
+#git pull
+#cd ../
+#rm -rf manger-deploy/*
+cp -r manger2/dist/ manger-deploy/
 cd manger-deploy
 git add .
 git commit -m "update"
