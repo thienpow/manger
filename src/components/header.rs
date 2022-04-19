9191f2bca95c35a8f1cf5c04b8f9273801f6f987
Ken Chong <thienpow@aadi.my>
2022-04-19 21:26:55 +0800

    restructure www

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,15 +1,12 @@
-
-
 use sycamore::prelude::*;
 
 use crate::components::safearea::SafeArea;
 use crate::route::AppRoutes;
-use crate::store::{CurrentRoute};
-use crate::svg::{NOTIF_SVG};
+use crate::store::CurrentRoute;
+use crate::svg::NOTIF_SVG;
 
 #[component]
 pub fn Header<G: Html>(cx: Scope) -> View<G> {
-
     let CurrentRoute(current_route) = use_context::<CurrentRoute>(cx);
 
     let header_ref = create_node_ref(cx);
@@ -29,7 +26,7 @@ pub fn Header<G: Html>(cx: Scope) -> View<G> {
             div(ref=left_menu_ref, class="header-menu-left") {
                 a(class="logo", aria-label="Manger Home Page", href="/") {
                     img(loading="lazy", alt="Manger Logo", src="/assets/img/logo-192.png", width="100%")
-                } 
+                }
             }
             div(class="menu-gap")
             div(class="header-menu") {
@@ -37,21 +34,21 @@ pub fn Header<G: Html>(cx: Scope) -> View<G> {
                 a(class=(match *current_route.get() {AppRoutes::Bible => "is-active", _ => ""}), href="/bible") {"Bible"}
                 a(class=(match *current_route.get() {AppRoutes::Community => "is-active", _ => ""}), href="/community") {"Community"}
             }
-            
+
             div(class="search-bar") {
                 input(type="text", placeholder="Search", on:focus=on_search_focus, on:blur=on_search_blur)
             }
             div(class="menu-gap")
             div(class="header-profile") {
                 div(class="notification") {
-                    span(dangerously_set_inner_html=NOTIF_SVG) 
+                    span(dangerously_set_inner_html=NOTIF_SVG)
                     span(class="notification-number") {(10)}
                 }
                 div(class="icon-gap")
                 div() {
                     a(class=(match *current_route.get() {AppRoutes::Profile => "is-active", _ => ""}), href="/profile", alt="Profile") {
                         img(class="profile-img", loading="lazy",
-                        src="/assets/img/avatar_1.webp", 
+                        src="/assets/img/avatar_1.webp",
                         alt="My Profile")
                     }
                 }
